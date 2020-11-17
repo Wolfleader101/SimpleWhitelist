b77d1df82e3554dfb2be08d7180a010798bc206c
Wolfleader <james.morgan@iinet.net.au>
2020-11-17 10:52:27 +0800

    basic command

--- SimpleWhitelist.cs
+++ SimpleWhitelist.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using Oxide.Core.Libraries.Covalence;
 using UnityEngine;
 
 namespace Oxide.Plugins
@@ -24,10 +25,65 @@ namespace Oxide.Plugins
 
             permission.RegisterPermission(WhitelistPerms, this);
         }
+        
+        private void OnPlayerConnected(IPlayer player)
+        {
+            PlayerData playerData = new PlayerData(player);
+            // BasePlayer basePlayer = player.Object as BasePlayer;
+            var foundPlayer = config.whitelisted.Find(ply => ply.steamId == player.Id);
+            if (foundPlayer == null)
+            {
+                player.Kick("You are not whitelisted");
+            }
+        }
 
         #endregion
 
         #region Methods
+
+        private void AddCommand(string steamIDString)
+        {
+            
+        }
+
+        private void RemoveCommand(string steamIDString)
+        {
+            
+        }
+
+        private void HelpCommand()
+        {
+            
+        }
+        #endregion
+
+        #region Commands
+
+        [Command("whitelist")]
+        private void WhitelistCommand(IPlayer player, string command, string[] args)
+        {
+            if(!player.HasPermission(WhitelistPerms)) return;
+            if (args.Length > 2)
+            {
+                HelpCommand();
+                return;
+            }
+
+                switch (args[0])
+            {
+                case "add":
+                    AddCommand(args[1]);
+                    break;
+                case "remove":
+                    RemoveCommand();
+                    break;
+                case "help":
+                    break;
+                default:
+                    HelpCommand();
+                    break;
+            }
+        }
 
         #endregion
 
@@ -35,20 +91,43 @@ namespace Oxide.Plugins
 
         private class PluginConfig
         {
-            //[JsonProperty("Enderpearl")] public string enderpearl { get; set; }
+            [JsonProperty("Whitelisted")] public List<PlayerData> whitelisted { get; set; }
         }
 
         private PluginConfig GetDefaultConfig()
         {
             return new PluginConfig
             {
-              //  enderpearl = "snowball.entity"
+              whitelisted = new List<PlayerData>()
             };
         }
 
         protected override void LoadDefaultConfig()
         {
             Config.WriteObject(GetDefaultConfig(), true);
+        }
+
+        #endregion
+
+        #region Classes
+        private class PlayerData
+        {
+            public string name { get; set; }
+            public string steamId { get; set; }
+
+            public PlayerData() { }
+
+            public PlayerData(IPlayer player)
+            {
+                name = player.Name;
+                steamId = player.Id;
+            }
+
+            public PlayerData(BasePlayer player)
+            {
+                name = player.displayName;
+                steamId = player.UserIDString;
+            }
         }
 
         #endregion
